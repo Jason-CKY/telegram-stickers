1d360d14a03af8db6c2bd46c07323ccc987cd294
Jason Cheng <jasoncky96@gmail.com>
2022-04-03 21:52:14 +0800

    added catjam

--- convert.py
+++ convert.py
@@ -1,11 +1,10 @@
 import argparse
 import os
 from utils import *
+from pathlib import Path
 
 conversion_mappings = {
     ".png": png_to_webm,
-    '.jpg': jpg_to_webm,
-    '.jpeg': jpg_to_webm,
     '.gif': gif_to_webm
 }
 
@@ -17,13 +16,13 @@ def parse_arguments():
 def convert_to_webm(dir: str):
     files = os.listdir(os.path.join(dir, 'src'))
     for file in files:
-        ext = f".{file.split('.')[-1]}"
-        fpath = os.path.join(dir, 'src', file)
+        fpath = Path(os.path.join(dir, 'src', file))
+        ext = fpath.suffix
         if ext in conversion_mappings.keys():
             print(f"Converting {file} to .webm format...")
             conversion_mappings[ext](fpath)
+            os.remove(fpath)
             
-
 def main():
     args = parse_arguments()
     for dir in args.dir:
--- utils.py
+++ utils.py
@@ -1,15 +1,39 @@
+import os
+import ffmpeg
 from PIL import Image
+from pathlib import Path
 
 IMG_SIZE = (512, 512)
 
-def png_to_webm(fpath):
+def png_to_webm(fpath: Path):
     img = Image.open(fpath)
     img = img.resize(IMG_SIZE)
-    save_path = f"{fpath.split('.')[0]}512.png"
-    img.save(save_path)
+    img.save(fpath.with_suffix('.gif'))
+    gif_to_webm(fpath.with_suffix('.gif'))
+    os.remove(fpath.with_suffix('.gif'))
 
-def jpg_to_webm():
-    pass
+def gif_to_webm(fpath: Path):
+    job = ffmpeg.input(fpath).filter('fps', fps=30)
+    info = ffmpeg.probe(fpath)
+    stream = info['streams'][0]
+    fmt = info['format']
+    # Try to scale to 512px
+    if stream['width'] >= stream['height']:
+        job = job.filter('scale', 512, -1)
+    else:
+        job = job.filter('scale', -1, 512)
+    if 'duration' in fmt:
+        duration = float(fmt['duration'])
 
-def gif_to_webm():
-    pass
+        # Try speed up video if it's over 3 seconds
+        if duration > 3.0:
+            job = job.filter('setpts', f"(2.9/{duration})*PTS")
+    else:
+        job = job.filter('setpts', f"1.0*PTS")
+
+    job.output(str(fpath.with_suffix('.webm')),
+            pix_fmt='yuva420p',
+            vcodec='libvpx-vp9',
+            an=None,  # Remove Audio
+            ).overwrite_output().run()
+        
